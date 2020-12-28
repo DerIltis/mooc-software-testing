bb7427652fb14335da54387d9e5195949e740c84
DerIltis <der_iltis@web.de>
2020-12-28 07:28:24 +0100

    * Introduced BeforeEach method to unify th setup part of tests in MyRomanNumeralTest

--- src/test/java/tudelft/roman/MyRomanNumeralTest.java
+++ src/test/java/tudelft/roman/MyRomanNumeralTest.java
@@ -1,37 +1,42 @@
 package tudelft.roman;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.naming.OperationNotSupportedException;
 
 public class MyRomanNumeralTest {
 
+    private MyRomanNumeral roman;
+    @BeforeEach
+    private void initialize()
+    {
+        roman = new MyRomanNumeral();
+
+    }
+
 
     @Test
     public void singleNumber1() throws OperationNotSupportedException {
-        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("I");
         Assertions.assertEquals(1, result);
     }
 
     @Test
     public void singleNumber2() throws OperationNotSupportedException {
-        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("V");
         Assertions.assertEquals(5, result);
     }
 
     @Test
     public void manyNumber1() throws OperationNotSupportedException {
-        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("III");
         Assertions.assertEquals(3, result);
     }
 
     @Test
     public void manyNumber2() throws OperationNotSupportedException {
-        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("XX");
         Assertions.assertEquals(20, result);
     }
@@ -39,49 +44,42 @@ public class MyRomanNumeralTest {
     @Test
     public void tooManyNumbers() throws OperationNotSupportedException {
         // TODO: This should be invalid!
-        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("IIII");
         Assertions.assertEquals(4, result);
     }
 
     @Test
     public void numberWithManyDigits1() throws OperationNotSupportedException {
-        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("VIII");
         Assertions.assertEquals(8, result);
     }
 
     @Test
     public void numberWithManyDigits2() throws OperationNotSupportedException {
-        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("XVIII");
         Assertions.assertEquals(18, result);
     }
 
     @Test
     public void numberWithSubtractiveNotation() throws OperationNotSupportedException {
-        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("IV");
         Assertions.assertEquals(4, result);
     }
 
     @Test
     public void numberWithManySubtractiveNotation() throws OperationNotSupportedException {
-        MyRomanNumeral roman = new MyRomanNumeral();
         int result = roman.convert("XCIV");
         Assertions.assertEquals(94, result);
     }
 
     @Test
     public void invalidCharInNumeralArgument(){
-        MyRomanNumeral roman = new MyRomanNumeral();
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> { int result = roman.convert("VBII");});
     }
 
     @Test
     public void invalidOrderInNumeralArgument1(){
-        MyRomanNumeral roman = new MyRomanNumeral();
 
         Assertions.assertThrows(OperationNotSupportedException.class, () -> { int result = roman.convert("VV");});
     }
@@ -98,8 +96,7 @@ public class MyRomanNumeralTest {
 
 
     @Test
-    public void numberWithAndWithoutSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
+    public void numberWithAndWithoutSubtractiveNotation() throws OperationNotSupportedException {
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
     }
