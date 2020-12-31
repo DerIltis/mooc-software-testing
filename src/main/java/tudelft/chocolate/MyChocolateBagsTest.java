21934ffb25ac8ca115c28b8b89442c84cc2545c5
DerIltis <der_iltis@web.de>
2020-12-31 10:55:09 +0100

    Started Topic 2 - Functional Testing * Added a parameterized test in CHocolateBags Project

--- src/main/java/tudelft/chocolate/MyChocolateBagsTest.java
+++ src/main/java/tudelft/chocolate/MyChocolateBagsTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class ChocolateBagsTest {
+public class MyChocolateBagsTest {
     @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
     @CsvSource({ "1,1,5,0", "1,1,6,1", "1,1,7,-1", "1,1,8,-1" })
     public void totalIsTooBig(int small, int big, int total, int expectedResult) {
@@ -22,8 +22,8 @@ public class ChocolateBagsTest {
 
     @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
     @CsvSource({
-        "0,3,17,-1", "1,3,17,-1", "2,3,17,2", "3,3,17,2",
-        "0,3,12,-1", "1,3,12,-1", "2,3,12,2", "3,3,12,2"})
+            "0,3,17,-1", "1,3,17,-1", "2,3,17,2", "3,3,17,2",
+            "0,3,12,-1", "1,3,12,-1", "2,3,12,2", "3,3,12,2"})
     public void bigAndSmallBars(int small, int big, int total, int expectedResult) {
         int result = new ChocolateBags().calculate(small, big, total);
         Assertions.assertEquals(expectedResult, result);
@@ -36,5 +36,10 @@ public class ChocolateBagsTest {
         Assertions.assertEquals(expectedResult, result);
     }
 
-
+    @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
+    @CsvSource({ "4,1,8,3", "3,2,8,3", "2,1,8,-1", "1,1,8,-1" })
+    public void insufficientSmallBars(int small, int big, int total, int expectedResult) {
+        int result = new ChocolateBags().calculate(small, big, total);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
