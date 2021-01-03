1179f09ca7df8d1a23e759e26d41080d148dc564
DerIltis <der_iltis@web.de>
2021-01-03 13:44:58 +0100

    Finished Topic 4 (Testability and mock objects) * Added Tests for DiscountApplierTest using a mock object and fixed the bug

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,30 @@
 package tudelft.discount;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import tudelft.invoicemocked.InvoiceDao;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiscountApplierTest {
+
+    @Test
+    public void SimpleTest()
+    {
+        ProductDao dao = Mockito.mock(ProductDao.class);
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Foo", 1, "HOME"));
+        products.add(new Product("Bar", 1, "BUSINESS"));
+        Mockito.when(dao.all()).thenReturn(products);
+
+        DiscountApplier applier = new DiscountApplier(dao);
+        applier.setNewPrices();
+
+        Assertions.assertEquals(0.9, products.get(0).getPrice());
+        Assertions.assertEquals(1.1, products.get(1).getPrice());
+    }
 
 }
